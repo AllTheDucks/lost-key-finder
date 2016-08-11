e74963229439aca8ee9021bbb42716b9bed5ea19
Harry Scells <harryscells@gmail.com>
2016-08-12 09:24:30 +1000

    more filtering of files

--- src/com/alltheducks/lkf/LanguageBundleProcessor.java
+++ src/com/alltheducks/lkf/LanguageBundleProcessor.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
@@ -25,6 +27,16 @@ public class LanguageBundleProcessor {
         this.filename = filename;
     }
 
+    private boolean isBinaryFile(Path path) {
+        String name = path.toString();
+        return name.endsWith(".jar")
+                || name.endsWith(".war")
+                || name.endsWith(".zip")
+                || name.endsWith(".png")
+                || name.endsWith(".jpg")
+                || name.endsWith(".gif");
+    }
+
     /**
      * Process the language pack for a given directory. This method loops over each
      * key in the language pack and then over each file in the directory tree.
@@ -35,31 +47,51 @@ public class LanguageBundleProcessor {
         if (!Files.exists(directory)) {
             return;
         }
-
         // the properties file should have at least one property
         if (properties.keySet().size() > 0) {
+            // create the list of files once per language pack
+            List<Path> files = new ArrayList<>();
+            try {
+                files = Files.walk(directory, FileVisitOption.FOLLOW_LINKS)
+                        // no directories
+                        .filter(path -> !Files.isDirectory(path))
+                        // don't look at itself
+                        .filter(path -> !path.getFileName().equals(filename.getFileName()))
+                        // don't look at dotfiles and dotdirs
+                        .filter(path -> !path.toString().startsWith("."))
+                        // don't care about binary files
+                        .filter(path -> !isBinaryFile(path))
+                        .collect(Collectors.toList());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            // iterate over each of the property keys
             for (Object o : properties.keySet()) {
-                boolean found = false;
                 String languageKey = o.toString();
-                try {
-                    // walk the directory
-                    for (Path path : Files.walk(directory, FileVisitOption.FOLLOW_LINKS).collect(Collectors.toList())) {
-                        // ignore directories and the current properties file
-                        if (!Files.isDirectory(path) && !path.getFileName().equals(filename.getFileName())) {
-                            // test if the language pack key occurs in the file
-                            String content = new String(Files.readAllBytes(path));
-                            if (content.contains(languageKey)) {
-                                found = true;
-                            }
+
+                    // represents the presence of a language key
+                    boolean found = false;
+
+                    // loop over the files we now have in memory
+                    for (Path file : files) {
+                        String content = "";
+                        try {
+                            content = new String(Files.readAllBytes(file));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                        // check if the language pack key is contained inside a file
+                        if (content.contains(languageKey)) {
+                            found = true;
                         }
                     }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                // notify the user of any keys that were not found
-                if (!found) {
-                    System.out.println(String.format("%s: %s", filename, languageKey));
-                }
+                    // notify the user of any keys that were not found
+                    if (!found) {
+                        System.out.println(String.format("%s: %s", filename, languageKey));
+                    }
+
             }
         }
     }
--- src/com/alltheducks/lkf/Main.java
+++ src/com/alltheducks/lkf/Main.java
@@ -9,10 +9,23 @@ import java.nio.file.Paths;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        Path projectDirectory = Paths.get(".");
+        String projectDir = ".";
+        if (args.length == 1) {
+            projectDir = args[1];
+        }
+        Path projectDirectory = Paths.get(projectDir);
 
         // get a list of all the property files
         Files.walk(projectDirectory, FileVisitOption.FOLLOW_LINKS)
+                // don't care about any files in the ./build/ directory if a build.gradle file exists
+                .filter(path -> {
+                    if (Files.exists(Paths.get("build.gradle"))) {
+                        if (path.toString().startsWith("./build/")) {
+                            return false;
+                        }
+                    }
+                    return true;
+                })
                 // only care about property files
                 .filter(path -> path.toString().endsWith(".properties"))
                 // we want the parent directory to be bundles
